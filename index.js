69b5f4e53822303fcfb7ec24020ea6683fe64849
Vanes <vanes996@hotmail.it>
2018-12-05 12:01:33 +0100

    :art: final commit

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const url=require('url')
 const app = express();
 
 const PORT = process.env.PORT || 3000
@@ -8,16 +7,9 @@ const PORT = process.env.PORT || 3000
 app.get('/', (req, res) => res.status(200).send('Hello World!'));
 
 app.get('/square', async(req,res) =>{
-
-console.log(req.query.string.length**2);
-let result = await string_square(req.query.string);
-
-res.status(200).json(result);
-
-
-//res.status(200).send(req.params.string);
-
-
+    console.log(req.query.string.length**2);  
+    let result = await string_square(req.query.string);
+    res.status(200).json(result);
 });
 
 let server = app.listen(PORT, () => console.log('Example app listening on port '+ PORT));
